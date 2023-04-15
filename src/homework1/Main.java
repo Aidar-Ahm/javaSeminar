784206a4b6a701e3fbb19a3f4e2c1122c6c72374
Aidar-Ahm <aidar.akhmedzyanov@gmail.com>
2023-04-15 12:48:10 +0500

    homework1

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -2,11 +2,13 @@ package homework1;
 
 import homework1.task.Task1;
 import homework1.task.Task2;
+import homework1.task.Task3;
 
 public class Main {
     public static void main(String[] args) {
         //   Task1.triangle()
-        Task2.simple();
+        //   Task2.simple();
+        Task3.calc();
 
     }
 }
