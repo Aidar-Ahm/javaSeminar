87f36e386ac565c9c0beb82e9cb3d1ab4e4b72c0
Aidar-Ahm <aidar.akhmedzyanov@gmail.com>
2023-04-29 22:25:07 +0500

    homework6

--- src/homework6/task/LaptopStore.java
+++ src/homework6/task/LaptopStore.java
@@ -5,20 +5,18 @@ import java.util.*;
 
 public class LaptopStore {
         public static void main(String[]args) {
-            Laptop laptop1 = new Laptop("Samsung", "S102", "Windows", 8, 256);
-            Laptop laptop2 = new Laptop("Aplle", "MacBook", "MacOS", 8, 256);
-            Laptop laptop3 = new Laptop("HP", "HP15", "Linux", 16, 512);
-            Laptop laptop4 = new Laptop("HP", "HP15", "Linux", 2, 512);
-            Laptop laptop5 = new Laptop("HP", "HP15", "Linux", 4, 512);
+        Laptop laptop1 = new Laptop("Samsung", "S102", "Windows", 8, 256);
+        Laptop laptop2 = new Laptop("Aplle", "MacBook", "MacOS", 8, 256);
+        Laptop laptop3 = new Laptop("HP", "HP15", "Linux", 16, 512);
+        Laptop laptop4 = new Laptop("HP", "HP15", "Linux", 2, 512);
+        Laptop laptop5 = new Laptop("HP", "HP15", "Linux", 4, 512);
 
-
-            Set<Laptop> Laptops = new HashSet<Laptop>();
-            Laptops.add(laptop1);
-            Laptops.add(laptop2);
-            Laptops.add(laptop3);
-            Laptops.add(laptop4);
-            Laptops.add(laptop5);
-
+        Set<Laptop> Laptops = new HashSet<Laptop>();
+        Laptops.add(laptop1);
+        Laptops.add(laptop2);
+        Laptops.add(laptop3);
+        Laptops.add(laptop4);
+        Laptops.add(laptop5);
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите характеристики: 1. Бренд : ");
@@ -33,6 +31,5 @@ public class LaptopStore {
                 System.out.println(lap.outputLaptop());
             }
         }
-        sc.close();
     }
 }
